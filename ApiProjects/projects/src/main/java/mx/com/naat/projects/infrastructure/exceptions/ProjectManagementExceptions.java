36147e16ddd7eaefb246be1d8da4ea84e02c7b29
fvera <fvera@MacBook-Pro-de-MBP-361.local>
2021-10-16 13:53:29 -0500

    correction on management exception

--- ApiProjects/projects/src/main/java/mx/com/naat/projects/infrastructure/exceptions/ProjectManagementExceptions.java
+++ ApiProjects/projects/src/main/java/mx/com/naat/projects/infrastructure/exceptions/ProjectManagementExceptions.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice(basePackages = "com.example.demo")
+@ControllerAdvice(basePackages = "mx.com.naat")
 public class ProjectManagementExceptions {
 	@ExceptionHandler(value = {KeyExists.class})
 	public ResponseEntity<Object> handlerKeyExists(KeyExists ex){
