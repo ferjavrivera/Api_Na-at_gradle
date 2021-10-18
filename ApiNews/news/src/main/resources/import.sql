23fdd22c17d43f88ba80c137934da8e2f448ef49
ferjavrivera <91631384+ferjavrivera@users.noreply.github.com>
2021-10-18 14:01:17 -0500

    Create import.sql
    sql noticias

---
+++ ApiNews/news/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO News(headline,summary,news_body,author_id) VALUES ('¿Sabes cómo lavarte las manos?',
+    'n maximus, tellus et ultricies venenatis, sapien erat semper arcu, vel gravida tellus nulla non lorem. Duis pulvinar massa in.',
+    'Curabitur a dolor venenatis, imperdiet lacus vitae, euismod purus. Sed blandit quis tellus sed cursus. Nullam at turpis non dolor eleifend finibus. Proin ultricies leo eget ipsum vehicula posuere. ',
+    unhex('00000000000000000000000000000000'));
+    
+    
+INSERT INTO News(headline,summary,news_body,author_id) VALUES ('Fundación Banco de Tapitas',
+    'Phasellus euismod molestie odio, a consectetur enim maximus sed. Morbi iaculis nisi vestibulum interdum pulvinar. Vestibulum sodales ipsum dignissim, tempor.',
+    'Ut maximus suscipit volutpat. Nullam vel laoreet massa. Nunc ut tincidunt ex, eget faucibus est. Etiam ut elementum turpis. Nunc sem nunc, viverra eu accumsan a, eleifend ut neque. Curabitur tincidunt',
+    unhex('00000000000000000000000000000000'));
+    
+INSERT INTO News(headline,summary,news_body,author_id) VALUES ('No bajemos la guardia',
+    'Nulla mauris quam, mollis quis nisi et, semper luctus dolor. Donec blandit efficitur magna, eu volutpat lacus luctus id. Donec.',
+    'Maecenas nec enim ante. Sed consectetur, diam nec maximus mollis, purus urna lobortis urna, faucibus tincidunt enim nunc ut felis. Praesent eget tincidunt ex. Aliquam sodales nisi lacus, ut vestibulum.',
+    unhex('00000000000000000000000000000000'));
