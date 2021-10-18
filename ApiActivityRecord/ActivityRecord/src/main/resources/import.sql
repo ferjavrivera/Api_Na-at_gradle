3cece4643447fc8befdac8d31e3a8b3d96c12aae
ferjavrivera <91631384+ferjavrivera@users.noreply.github.com>
2021-10-18 14:27:07 -0500

    Create import.sql
    import.sql

---
+++ ApiActivityRecord/ActivityRecord/src/main/resources/import.sql
@@ -0,0 +1,34 @@
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE1'),
+                                        unhex('00000000000000000000000000000001'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE1'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE2'),
+                                        unhex('00000000000000000000000000000002'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE2'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE3'),
+                                        unhex('00000000000000000000000000000003'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE3'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE4'),
+                                        unhex('00000000000000000000000000000004'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE4'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE5'),
+                                        unhex('00000000000000000000000000000005'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE5'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE6'),
+                                        unhex('00000000000000000000000000000006'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE6'));
+
+INSERT INTO ActivityRecord (activity_duration, activity_record_date,activity_record_enabled,project_id, user_id, activity_id)
+VALUES              ('2','2020-08-26',1,unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE7'),
+                                        unhex('00000000000000000000000000000007'),
+                                        unhex('AAAAAAAABBBBCCCCDDDDEEEEEEEEEEE7'));
