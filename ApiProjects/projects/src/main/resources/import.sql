67d85647b39fb5a386e13c28277f75d7e34922bd
ferjavrivera <91631384+ferjavrivera@users.noreply.github.com>
2021-10-18 14:23:46 -0500

    Create import.sql
    import.sql

---
+++ ApiProjects/projects/src/main/resources/import.sql
@@ -0,0 +1,59 @@
+INSERT INTO Projects (project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE1'), "PROJECT_X","Project X",
+
+"Proyecto super asombroso", 1,unhex('FE3F7DBF751545C29D31F8A7658CDB13'),unhex('00000000000000000000000000000001'));
+
+
+
+INSERT INTO Projects ( project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE2'), "PROJECT_A","Project A",
+
+"Proyecto para usuarios", 1,unhex('FE3F7DBF751545C29D31F8A7658CDB14'),unhex('00000000000000000000000000000002'));
+
+
+
+
+INSERT INTO Projects (project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE3'), "PROJECT_B","Project B",
+
+"Proyecto para clientes", 1,unhex('FE3F7DBF751545C29D31F8A7658CDB15'),unhex('00000000000000000000000000000003'));
+
+
+
+
+INSERT INTO Projects (project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE4'), "PROJECT_C","Project C",
+
+"Proyecto para noticias", 1,unhex('FE3F7DBF751545C29D31F8A7658CDB16'),unhex('00000000000000000000000000000004'));
+
+
+
+
+INSERT INTO Projects (project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE5'), "PROJECT_D","Project D",
+
+"Proyecto para proyectos",1,unhex('FE3F7DBF751545C29D31F8A7658CDB17'),unhex('00000000000000000000000000000005'));
+
+
+
+
+INSERT INTO Projects (project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE6'), "PROJECT_E","Project E",
+
+"Proyecto para activity record",1,unhex('FE3F7DBF751545C29D31F8A7658CDB18'),unhex('00000000000000000000000000000006'));
+
+
+
+
+INSERT INTO Projects ( project_id, project_key, project_name, description, project_enabled, client_id, author_id)
+
+VALUES ( unhex('CAAAAAAABBBBCCCCDDDDEEEEEEEEEEE7'), "PROJECT_F","Project F",
+
+"Proyecto para dockerizar",1,unhex('FE3F7DBF751545C29D31F8A7658CDB19'),unhex('00000000000000000000000000000007'));
+
