13a70ec73d38a459e63b41810394f51cb1316755
vmorales <vmorales@na-at.com.mx>
2021-10-18 13:33:13 -0500

    Update Entity Activity

--- ApiActivities/activities/src/main/java/mx/com/naat/activities/infrastructure/entity/Activities.java
+++ ApiActivities/activities/src/main/java/mx/com/naat/activities/infrastructure/entity/Activities.java
@@ -1,6 +1,8 @@
 package mx.com.naat.activities.infrastructure.entity;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,25 +30,25 @@ public class Activities {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
-	@Column(name = "CREATION_DATE")
-	private LocalDateTime creationDate;
+	@Column(name = "CREATION_DATE", columnDefinition = "TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3)")
+	private Date creationDate;
 	
-	@Column(name = "MODIFICATION_DATE")
-	private LocalDateTime modificationDate;
+	@Column(name = "MODIFICATION_DATE", columnDefinition = "TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3)")
+	private Date modificationDate;
 	
 	@Column(name = "ACTIVITY_ENABLED" ,columnDefinition = "BOOLEAN DEFAULT TRUE")
 	private boolean enabled;
-
-	@Column(name = "AUTHOR_ID")
+	@NotNull
+	@Column(name = "AUTHOR_ID", nullable = false)
 	private UUID nameAuthor;
-	
-	@Column(name = "ACTIVITY_KEY")
+	@NotNull
+	@Column(name = "ACTIVITY_KEY", nullable = false)
 	private String key;
-	
-	@Column(name = "ACTIVITY_NAME")
+	@NotNull
+	@Column(name = "ACTIVITY_NAME", nullable = false)
 	private String name;
 	
-	@Column(name = "DESCRIPTION")
+	@Column(name = "DESCRIPTION", nullable = false)
 	private String description;
 
 
@@ -57,12 +60,12 @@ public class Activities {
 	@PrePersist
 	public void preinsert()
 	{
-		creationDate = LocalDateTime.now();
-		modificationDate = LocalDateTime.now();
+		/*creationDate = LocalDateTime.now();
+		modificationDate = LocalDateTime.now();*/
 	}
 	@PreUpdate
 	public void preupdate()
 	{
-		modificationDate = LocalDateTime.now();
+		//modificationDate = LocalDateTime.now();
 	}
 }
